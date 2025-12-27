e7e49d6a743ff5b8705be2ddc199411283cc634a
DavidFire07 <davidsoltys22@gmail.com>
2025-12-27 18:55:49 +0100

    idem nahrat novy priecinok

--- gitDavidSoltys/ipynb 5.12/json_conversion.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("zadanie.txt", "r", encoding="utf-8") as file:
-    notebook_json = json.load(file)
-
-with open("zadanie.ipynb", "w", encoding="utf-8") as file:
-    json.dump(notebook_json, file, indent=2)
-
-print("Notebook created: zadanie.ipynb")
